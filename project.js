00ed816f4e76b5dbf6fd88b7ff4011da9e05c8e9
Samson Eitan Lewin <samsoneitanlewin@calvisitor-10-105-181-142.calvisitor.1918.berkeley.edu>
2017-03-22 21:56:03 -0700

    latest styling

--- project.js
+++ project.js
@@ -25,7 +25,7 @@ app.controller("myCtrl", function($scope,$http){
 			console.log("Longitude is " + response.data.results[0].geometry.location.lng);
 
 			var app = angular.module("myApp",[]);
-			var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+addressP+"&location="+response.data.results[0].geometry.location.lat+","+response.data.results[0].geometry.location.lng+"&radius=8406&key=AIzaSyB0TQy5qiEIcvgm0puF1VdzKCbMXu1tgPA"
+			var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+addressP+"&location="+response.data.results[0].geometry.location.lat+","+response.data.results[0].geometry.location.lng+"&radius=8406&key=AIzaSyAG6bdd9sFybP1JBjw934o7KQhkPm1-s9k"
 ;
 
 			$http({
@@ -36,7 +36,7 @@ app.controller("myCtrl", function($scope,$http){
 			console.log("HELLO", childResponse.data);
 			var photoReference = childResponse.data.results[0].photos[0].photo_reference;
 
-			var imageURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photoreference="+photoReference+"&key=AIzaSyB0TQy5qiEIcvgm0puF1VdzKCbMXu1tgPA"
+			var imageURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photoreference="+photoReference+"&key=AIzaSyAG6bdd9sFybP1JBjw934o7KQhkPm1-s9k"
 
 			$http({
 				method: "GET",
@@ -56,14 +56,11 @@ app.controller("myCtrl", function($scope,$http){
 
 			var widget = $("<div id='widget'></div>");
 			widget.css({
-			    position: 'absolute',
-    			top: '48%',
-    			left: '35%',
 			});
-			$("#background").append(widget);
+			$("#widget").append(widget);
 			widget.addClass("widget");
 			_aqiFeed({    
-  display:"<div style='color:#ffffff;max-width:180px;text-align:center;'><small>Air Quality Index:</small> <div style='font-size:88px;height:100px;padding-bottom:30px;'>%aqiv</div> %impact</div>",  
+  display:"<div style='color:#ffffff;max-width:180px;text-align:center;'><div style='font-size:80px;height:100px;padding-bottom:30px;'>%aqiv</div> %impact</div>",  
   container:"widget",    
   city: addressP
   });  
@@ -80,6 +77,21 @@ var weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat="+response.
 			var fahrenheitTemp = Math.round(temp*(9/5)-(459.67));
 			console.log(fahrenheitTemp);
 			$("#tempInfo").html(fahrenheitTemp+"°F");
+		});
+
+var theLat = (response.data.results[0].geometry.location.lat).toFixed(0);
+var theLong = (response.data.results[0].geometry.location.lng).toFixed(0);
+console.log(theLat);
+console.log(theLong);
+var uvURL = "http://api.openweathermap.org/v3/uvi/"+theLat+","+theLong+"/current.json?appid=aae9e9af552036e9f4bea0d156fa613d";
+console.log(uvURL);
+		$http({
+				method: "GET",
+				url: uvURL
+		}).then(function (fourthChildResponse){
+			
+			console.log(fourthChildResponse.data.data);
+			$("#uvInfo").html(fourthChildResponse.data.data);
 		})
 
 		});
